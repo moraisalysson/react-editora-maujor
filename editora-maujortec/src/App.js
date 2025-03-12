41b0a87a1e4279e2ecd2004882d88b5a5a6ecbf2
alysson.morais <alysson.morais@dataprev.gov.br>
2025-03-12 20:35:58 -0300

    segundo passo - trocando as imagens e add estilização

--- editora-maujortec/src/App.js
+++ editora-maujortec/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
 
 function App() {
   return (
     <>
-      <header className='topo'>Topo</header>
-      <main className='principal'>Principal</main>
-      <footer className='rodape'>Rodapé</footer>
+      <header className='topo'>
+        <h1 className="logo"></h1>
+        <ul>
+          <li><a href="">Navegação</a></li>
+        </ul>
+      </header>
+      <main className='principal'>
+        <h2>Últimos Lançamentos</h2>
+        <div className="card">Card</div>
+      </main>
+      <footer className='rodape'>
+        <p>Conteúdos cedidos pela Editora Novatec &#8212; Copyright 2020</p>
+      </footer>
     </>
   );
 }
