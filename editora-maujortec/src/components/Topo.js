def3b14d52cbcbd7b187f06cf589a389b8e8ad35
alysson.morais <alysson.morais@dataprev.gov.br>
2025-03-17 10:14:10 -0300

    capítulo 5 - componentes inicias

---
+++ editora-maujortec/src/components/Topo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "./Logo";
+import Navegacao from "./Navegacao";
+
+const Topo = () => {
+    return (
+        <>
+            <Logo />
+            <Navegacao />
+        </>
+    )
+}
+
+export default Topo;
